8f9468fbea8f28ca5dbbd9c8d2ceb9c4fb192795
AshwinChandapur <01fe18bar011@kletech.ac.in>
2022-02-12 15:55:07 +0530

    commit

--- 3_Implementation/tic tac toe.c
+++ 3_Implementation/tic tac toe.c
@@ -14,7 +14,7 @@ void start();
 void if_draw();
 void create_xotable();
 void player_1();
-void put_X_O(char ch,int pos);
+void mark_XO(char ch,int pos);
 COORD coord= {0,0}; 
 
 void gotoxy(int x,int y)
@@ -142,6 +142,59 @@ int winpos(int p)
     }
     return 0;
 }
+
+//creating the tictactoe board
+void create_xotable()
+{
+    int j;
+
+    for(j=9; j<17; j++)
+    {
+        gotoxy(35,j);
+        printf("|       |");
+    }
+    gotoxy(28,11);
+    printf("-----------------------");
+    gotoxy(28,14);
+    printf("-----------------------");
+    for(j=1; j<10; j++)
+    {
+        if(board[j] == 3)
+            mark_XO('X',j);
+        else if(board[j] == 5)
+            mark_XO('O',j);
+    }
+}
+
+void mark_XO(char ch,int pos)
+{
+    int m;
+    int x = 31, y = 10;
+    m = pos;
+    if(m > 3)
+    {
+        while(m > 3)
+        {
+            y += 3;
+            m -= 3;
+        }
+    }
+    if(pos % 3 == 0)
+        x += 16;
+    else
+    {
+        pos %= 3;
+        pos--;
+        while(pos)
+        {
+            x+=8;
+            pos--;
+        }
+    }
+    gotoxy(x,y);
+    printf("%c",ch);
+}
+
 void go(int n)
 {
     if(turn % 2)
@@ -213,56 +266,4 @@ void if_draw()
         getch();
         exit(0);
     }
-}
-
-//creating the tictactoe board
-void create_xotable()
-{
-    int j;
-
-    for(j=9; j<17; j++)
-    {
-        gotoxy(35,j);
-        printf("|       |");
-    }
-    gotoxy(28,11);
-    printf("-----------------------");
-    gotoxy(28,14);
-    printf("-----------------------");
-    for(j=1; j<10; j++)
-    {
-        if(board[j] == 3)
-            put_X_O('X',j);
-        else if(board[j] == 5)
-            put_X_O('O',j);
-    }
-}
-
-void put_X_O(char ch,int pos)
-{
-    int m;
-    int x = 31, y = 10;
-    m = pos;
-    if(m > 3)
-    {
-        while(m > 3)
-        {
-            y += 3;
-            m -= 3;
-        }
-    }
-    if(pos % 3 == 0)
-        x += 16;
-    else
-    {
-        pos %= 3;
-        pos--;
-        while(pos)
-        {
-            x+=8;
-            pos--;
-        }
-    }
-    gotoxy(x,y);
-    printf("%c",ch);
 }
