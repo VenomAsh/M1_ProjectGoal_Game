7b7357f8b0e3c47b89185be9ad3da893bfa51886
AshwinChandapur <01fe18bar011@kletech.ac.in>
2022-03-03 18:46:54 +0530

    commit

--- 3_Implementation/inc/tictactoe.h
+++ 3_Implementation/inc/tictactoe.h
@@ -1,7 +1,9 @@
+#ifndef tictactoe_H_
+#define tictactoe_H_
 #include<stdio.h>
-#include<conio.h>
 
-char square[10] = { 'o', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+char square[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
 int checkwin();
 int board();
+#endif
--- 3_Implementation/src/main.c
+++ 3_Implementation/src/main.c
@@ -1,5 +1,15 @@
-#include<stdio.h>
-#include"tictactoe.h"
+/**
+ * @file main.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-03
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include "tictactoe.h"
 int main()
 {
     int player = 1, i, choice;
